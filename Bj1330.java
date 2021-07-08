e56a3d81c3e12825a2ed81673771b6873c57b07d
202010800jaeheonLee <81469092+202010800jaeheonLee@users.noreply.github.com>
2021-07-09 01:26:55 +0900

    Add files via upload

---
+++ Bj1330.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Bj1330 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        if (A>B){System.out.println(">");}
+        else if (A<B){System.out.println("<");}
+        else if (A==B){System.out.println("==");}
+    }
+}
