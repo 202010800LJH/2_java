199ca9e5fddf484414773562c393b1c682811eaa
202010800jaeheonLee <81469092+202010800jaeheonLee@users.noreply.github.com>
2021-07-08 22:28:01 +0900

    Add files via upload

---
+++ Bj1008.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Bj1008 {
+    public static void main(String[] args){
+        double result;
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextInt();
+        double B = sc.nextInt();
+        result = A/B;
+        System.out.println(result);
+    }
+}
