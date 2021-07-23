75d9ae934c0550df604556c973f0704440f27adf
202010800jaeheonLee <81469092+202010800jaeheonLee@users.noreply.github.com>
2021-07-23 11:38:14 +0900

    Add files via upload

---
+++ Bj_3_3053.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Bj_3_3053 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int R = sc.nextInt();
+        double rep = R*R*3.141593;
+        double ret = R*R*2;
+        System.out.println(rep);
+        System.out.println(ret);
+    }
+}
