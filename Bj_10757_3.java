7eb9674df9c0cd31c67ff4433d0735c66af74d26
202010800jaeheonLee <81469092+202010800jaeheonLee@users.noreply.github.com>
2021-07-23 09:37:07 +0900

    Add files via upload

---
+++ Bj_10757_3.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Bj_10757_3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str_A = sc.next();
+        String str_B = sc.next();
+
+        //두 개의 수 중 가장 신 자리수 길이 구함
+        int max_length = Math.max(str_A.length(), str_B.length());
+
+        int[] A = new int[max_length+1];
+        int[] B = new int[max_length+1];
+
+        for (int i=str_A.length()-1, idx=0; i>=0; i--, idx++){
+            A[idx] = str_A.charAt(i)-'0';
+        }
+        for (int i=str_B.length()-1, idx=0; i>=0; i--, idx++){
+            B[idx] = str_B.charAt(i)-'0';
+        }
+
+        for (int i=0; i<max_length; i++){
+            int value = A[i]+B[i];
+            A[i]=value%10;
+            A[i+1]+=(value/10);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        if (A[max_length]!=0){
+            sb.append(A[max_length]);
+        }
+        for (int i=max_length-1;i>=0;i--){
+            sb.append(A[i]);
+        }
+        System.out.println(sb);
+    }
+}
